261e28430a9ae6992e0116187a1bc851eebef98f
Alejandro Ruiz Campos <alexruiz29del12@gmail.com>
2022-04-08 14:03:51 +0200

    Proyecto tramitador agenda

---
+++ TramitadorAgenda/TramitacionONGD/src/main/java/es/juntadeandalucia/aacid/tramitacionongd/action/TareasCargarPaisesAction.java
@@ -0,0 +1,9 @@
+package es.juntadeandalucia.aacid.tramitacionongd.action;
+
+import org.apache.log4j.Logger;
+
+public class TareasCargarPaisesAction {
+
+  private final transient Logger log = Logger.getLogger(TareasConvocatoriaAction.class);
+
+}
